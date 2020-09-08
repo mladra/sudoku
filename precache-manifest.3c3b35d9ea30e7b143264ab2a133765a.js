25164c21b5e37af0678afd50e01c78c4052b0bec
mladra <michal.ladra.95@gmail.com>
2020-09-08 11:35:24 +0200

    Updates

---
+++ precache-manifest.3c3b35d9ea30e7b143264ab2a133765a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfe192f37d5944ed5dcea5a29e0ca6a",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "6d7b4e459f644e228d2d",
+    "url": "/sudoku/static/css/main.0fa5c8e1.chunk.css"
+  },
+  {
+    "revision": "fd92b1d91fb691721ed6",
+    "url": "/sudoku/static/js/2.e520c3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.e520c3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7b4e459f644e228d2d",
+    "url": "/sudoku/static/js/main.bc35b2c8.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  }
+]);
